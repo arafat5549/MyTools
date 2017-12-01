6d52f0ba52517a02bd41da22c676d5287453047c
王耀 <44947737@qq.com>
2017-12-01 12:01:32 +0800

    111

--- shell/ginit.sh
+++ shell/ginit.sh
@@ -4,6 +4,6 @@ git init
 git add README.md
 git add .
 git commit -m "Init Commit"
-git remote add origin git@github.com:arafat5549/$1.git
+git remote add origin git@192.168.0.133:wangyao/$1.git
 git push -u origin master
 echo "--------Git Init End--------"
